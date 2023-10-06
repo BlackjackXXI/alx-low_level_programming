8b45d65c9889def8ca7481ef253e3110b56a9c2a
BlackjackXXI <oussama.bouaoued@gmail.com>
2023-10-06 14:19:49 -0700

    push 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+void *malloc_checked(unsigned int size)
 {
-    char *original_string = "Best School"; // Define the original string
-    size_t string_length = strlen(original_string);
-    char *copied_string = (char *)malloc((string_length + 1) * sizeof(char));
+	void *ptr;
 
-    if (copied_string == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return (1);
-    }
-    strcpy(copied_string, original_string);
-    printf("Copied String: %s\n", copied_string);
-    free(copied_string);
-    return (0);
+	if (size <= 0)
+		exit(98);
+
+	ptr = malloc(size);
+	if (!ptr)
+		exit(98);
+	else
+		return (ptr);
 }
