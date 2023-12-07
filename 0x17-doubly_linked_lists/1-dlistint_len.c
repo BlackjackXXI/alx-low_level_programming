5c10575564baf6e933ebda8dfe72fcbd5a2ae112
BlackjackXXI <oussama.bouaoued@gmail.com>
2023-12-07 05:04:19 -0800

    push´

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,14 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	const dlistint_t *node = h;
 	size_t i = 0;
 
-	while (h)
+	while (node)
 	{
-		h = h->next;
-		i++
+		i++;
+		node = node->next;
 	}
+	
 	return (i);
 }
